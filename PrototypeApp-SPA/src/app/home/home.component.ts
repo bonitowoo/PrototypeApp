4d3da381b7a0d5d417b74172bffa8ebcb881cdd9
bonito <bonitowoo@gmail.com>
2019-03-21 11:47:34 +0800

    end of section 6

--- PrototypeApp-SPA/src/app/home/home.component.ts
+++ PrototypeApp-SPA/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-home',
@@ -17,9 +16,6 @@ export class HomeComponent implements OnInit {
   registerToggle() {
     this.registerMode = true;
   }
-
-
-
   cancelRegisterMode(registerMode: boolean) {
     this.registerMode = registerMode;
   }
