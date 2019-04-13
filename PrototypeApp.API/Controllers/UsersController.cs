c4cb84aa5ad8e7f6eb7f9b12e03e865fdbb25921
bonito <bonitowoo@gmail.com>
2019-04-14 02:27:43 +0800

    End of Section 13

--- PrototypeApp.API/Controllers/UsersController.cs
+++ PrototypeApp.API/Controllers/UsersController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PrototypeApp.API.Data;
 using PrototypeApp.API.Dtos;
+using PrototypeApp.API.Helpers;
 
 namespace PrototypeApp.API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
